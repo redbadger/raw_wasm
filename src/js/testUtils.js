c7ff40d9b8ef1f43a5d2aa548e74a0eca5cf1618
Chris Whealy <chris.whealy@red-badger.com>
2021-10-12 13:39:59 +0100

    Add DocStrings

--- src/js/testUtils.js
+++ src/js/testUtils.js
@@ -1,10 +1,25 @@
 import { F32, F64 } from './wasmUtils.js'
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Yeah, they're equal-ish...
+/***********************************************************************************************************************
+ * Test whether the difference between two values is smaller than a given tolerance
+ *
+ * @param {number} tol - Tolerance value
+ * @param {number} val - The value received from the test output
+ * @param {number} req - The expected test output value
+ *
+ * @returns {boolean}
+ */
 const closeEnough = (tol, val, req) => req + tol >= val && req - tol <= val
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Test whether the receievd value and the expected value are within the acceptable tolerance for their datatype
+ *
+ * @param {WasmDatatype} datatype - WASM interface datatype
+ * @param {number}       actual   - Actual test output value
+ * @param {number}       expected - Expected test output value
+ *
+ * @returns {boolean}
+ */
 const isWithinTolerance =
   (datatype, actual, expected) =>
     datatype.label === F64.label
@@ -13,7 +28,25 @@ const isWithinTolerance =
       ? closeEnough(F32.tolerance, actual, expected)
       : actual === expected
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Check whether two arrays are equal. This test first requires that:
+ * a) Both arguments are arrays
+ * b) Both array arguments have identical length
+ *
+ * If this condition is satisfied, then a pointwise comparison of the corresponding array elements is performed
+ *
+ * @param {WasmDatatype[]} outTypes - An array holding the datatype of each element in the comparison arrays
+ * @param {number[]}       a1       - Array 1
+ * @param {number[]}       a2       - Array 2
+ *
+ * @returns {Object} A comparison object containing first, a boolean to indicate whether or not the two arrays are even
+ *                   comparable, and second, an array of objects where each object holds a pair of booleans.
+ *                   Each pair of booleans correspond to the comparison of the array elements at a given index.
+ *                   The following return values are possible:
+ *                   Strict equality                          = { "equal" : true,  "withinTolerance" : true }
+ *                   Not strictly equal, but within tolerance = { "equal" : false, "withinTolerance" : true }
+ *                   Not strictly equal and outside tolerance = { "equal" : false, "withinTolerance" : false }
+ */
 const checkArrayEquality =
   (outTypes, a1, a2) => {
     let comparison = {
@@ -28,7 +61,7 @@ const checkArrayEquality =
         (acc, val, idx) => {
           acc.push(
             val === a2[idx]
-            ? { "equal" : true, "withinTolerance" : false }
+            ? { "equal" : true, "withinTolerance" : true }
             : isWithinTolerance(outTypes[idx], val, a2[idx])
               ? { "equal" : false, "withinTolerance" : true }
               : { "equal" : false, "withinTolerance" : false }
@@ -43,13 +76,23 @@ const checkArrayEquality =
     return comparison
   }
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Perform a single test of a function exported from a WASM module
+ *
+ * @param {string} fnName        - The name of the function being tested
+ * @param {string} fnInstance    - The WASM module instance containing function {fnName}
+ * @param {string} fnOutputArity - The datatype(s) returned from the function being tested
+ * @param {string} testData      - The data to run this and the expected result(s)
+ *
+ * @returns {Object} An object containing a description of the test run and its outcome together with a count of the
+ *                   number of successful and failed tests
+ */
 const fnTester =
   (fnName, fnInstance, fnOutputArity, testData) => {
     let passed  = 0
     let failed  = 0
     let outcome = ""
-    let got     = fnInstance(...testData.whenPassed)
+    let got     = fnInstance(...testData.whenPassed)   // Test the function
 
     if (fnOutputArity.length === 1) got = [got]
 
@@ -76,7 +119,14 @@ const fnTester =
     }
   }
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Test all functions exported by a WASM module
+ *
+ * @param {boolean} runTests     - If set to false, all tests are bypassed
+ * @param {object}  wasmInstance - An executable WASM module instance
+ * @param {object}  wasmTestMap  - A Map object containing one or more tests for each exported function
+ * @param {boolean} showDetail   - Should detailed test results be displayed?
+ */
 const testWasm =
   (runTests, wasmInstance, wasmTestMap, showDetail) => {
     if (runTests) {
@@ -94,7 +144,7 @@ const testWasm =
 
           if (!fnTest) {
             ++acc.missing
-            test.description = `WARNING: No test found for exported WASM function '${exp}'`
+            test.description = `WARNING: No tests found for exported WASM function '${exp}'`
           }
           else {
             let fnName = fnTest.function.name
@@ -129,29 +179,37 @@ const testWasm =
     }
   }
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Write a WASM module test report to the browser console
+ *
+ * @param {Object}  testReport - An object containing the results of a WASM module's test run
+ * @param {boolean} showDetail - Should a detailed test report be produced?
+ */
 const showTestReport =
   (testReport, showDetail) => {
     // Write test summary
-    let total = testReport.passed + testReport.failed
+    let action  = 'log'
+    let msg     = 'All WASM exports tested'
+    let summary = `Performed ${testReport.passed + testReport.failed} tests: ${testReport.passed} passed, ${testReport.failed} failed`
 
-    if (testReport.missing)
-      console.warn(
-        `${testReport.missing} WASM export test${testReport.missing === 1 ? "" : "s"} missing`
-      )
-    else
-      console.log(`All WASM exports tested`)
+    if (testReport.missing) {
+      action = 'warn'
+      msg = `${testReport.missing} WASM export test${testReport.missing === 1 ? "" : "s"} missing`
+    }
 
-    console.log(
-      `Performed ${total} tests: ${testReport.passed} passed, ${testReport.failed} failed`
-    )
+    console[action](msg)
+    console.log(summary)
 
     // Optionally, write test details
     if (showDetail) showTestDetail(testReport)
   }
 
-// ---------------------------------------------------------------------------------------------------------------------
-const showTestDetail =
+/***********************************************************************************************************************
+ * Write a detailed WASM module test report to the browser console
+ *
+ * @param {Object}  An object containing the results of a WASM module's test run
+ */
+ const showTestDetail =
   testReport =>
     testReport
       .testList
@@ -164,7 +222,11 @@ const showTestDetail =
           )
       })
 
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Utility function to display the contents of the "host functions" object
+ *
+ * @params {Object} hostFns - The object containing all the host functions
+ */
 const showHostFns =
   hostFns =>
     Object
--- src/js/wasmUtils.js
+++ src/js/wasmUtils.js
@@ -1,47 +1,77 @@
-// These comparison tolerance values are somewhat arbitrary...
-const F64 = { "label": "f64", "tolerance" : 0.0000000000000005 }
-const F32 = { "label": "f32", "tolerance" : 0.0000000000000005 }
-const I64 = { "label": "i64", "tolerance" : 0 }
-const I32 = { "label": "i32", "tolerance" : 0 }
+/***********************************************************************************************************************
+ * WASM interface data types
+ *
+ * @constructor
+ * @param {string} label         - Datatype label
+ * @param {number} [tolerance=0] - Optional comparison tolerance value
+ */
+function WasmDatatype(label, tolerance) {
+  this.label = label
+  this.tolerance = tolerance || 0
+}
 
-// ---------------------------------------------------------------------------------------------------------------------
+// Due to floating point inaccuracies, comparing test results against expected values requires the use of a tolerance
+// values (these are somewhat arbitrary...)
+const F64 = new WasmDatatype("f64", 0.0000000000000005)
+const F32 = new WasmDatatype("f32", 0.0000000000000005)
+const I32 = new WasmDatatype("i32")
+
+/***********************************************************************************************************************
+ * Arbitrarily assign property `propName` of object `obj` to have the value `propVal`
+ * If `propName` already exists, its value is overwritten
+ *
+ * @param   {Object}  obj      - The object to be updated
+ * @param   {string}  propName - The object property being set
+ * @param   {*}       propVal  - The object property value
+ *
+ * @return  {object}  The updated object
+ */
 const setProperty = (obj, propName, propVal) => (_ => obj)(obj[propName] = propVal)
-const mergeObjects = (sourceObj, targetObj) => ({ ...targetObj, ...sourceObj })
 
-// ---------------------------------------------------------------------------------------------------------------------
-const TWO_F64_IN_ONE_F64_OUT  = {
-  "input"  : [F64, F64],
-  "output" : [F64]
+/***********************************************************************************************************************
+ * WASM function interface type
+ *
+ * @constructor
+ * @param {WasmDataType[]} inputTypes  - Array of WASM function input types
+ * @param {WasmDataType[]} outputTypes - Array of WASM function output types
+ */
+function WasmInterfaceType(inputTypes, outputTypes) {
+  this.input = inputTypes
+  this.output = outputTypes
 }
 
-const TWO_F64_IN_ONE_I32_OUT  = {
-  "input"  : [F64, F64],
-  "output" : [I32]
+/***********************************************************************************************************************
+ * Commonly used WASM function interface types
+ */
+const TWO_F64_IN_ONE_F64_OUT          = new WasmInterfaceType([F64, F64], [F64])
+const TWO_F64_IN_ONE_I32_OUT          = new WasmInterfaceType([F64, F64], [I32])
+const TWO_F64_ONE_I32_IN_ONE_I32_OUT  = new WasmInterfaceType([F64, F64, I32], [I32])
+const TWO_F64_IN_TWO_F64_OUT          = new WasmInterfaceType([F64, F64], [F64, F64])
+const FOUR_F64_IN_TWO_F64_OUT         = new WasmInterfaceType([F64, F64, F64, F64], [F64, F64])
+const FOUR_F64_ONE_I32_IN_ONE_I32_OUT = new WasmInterfaceType([F64, F64, F64, F64, I32], [I32])
+
+/***********************************************************************************************************************
+ * Return an object containing the pathname to a compiled WASM module and the name of the library into which its
+ * exported functions should be added
+ *
+ * @constructor
+ * @param {string} pathToWasmBin - Pathname to compiled WASM module
+ * @param {string} exportToLib   - The library name by which this WASM module's exported functions will be available
+ */
+function WasmModule(pathToWasmBin, exportToLib) {
+  this.pathToWasmBin = pathToWasmBin
+  this.exportToLib = exportToLib
 }
 
-const TWO_F64_ONE_I32_IN_ONE_I32_OUT  = {
-  "input"  : [F64, F64, I32],
-  "output" : [I32]
-}
-
-const TWO_F64_IN_TWO_F64_OUT  = {
-  "input"  : [F64, F64],
-  "output" : [F64, F64]
-}
-
-const FOUR_F64_IN_TWO_F64_OUT = {
-  "input"  : [F64, F64, F64, F64],
-  "output" : [F64, F64]
-}
-
-const FOUR_F64_ONE_I32_IN_ONE_I32_OUT = {
-  "input"  : [F64, F64, F64, F64, I32],
-  "output" : [I32]
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-// Unload all the exports of a WASM instance and add them to the possibly already existing `libName` property in the
-// `hostFns` object
+/***********************************************************************************************************************
+ * Add all the exports of a WASM instance to the possibly already existing `libName` property of object `hostFns`
+ *
+ * @param   {Object} wasmInstance - An instantiated WASM module that exports one or more functions
+ * @param   {string} libName      - The library name by which the exported functions are grouped
+ * @param   {string} hostFns      - The object acting as the repository for host functions
+ *
+ * @returns {Object} A new host functions object
+ */
 const packageWasmExports =
   (wasmInstance, libName, hostFns) =>
     Object
@@ -51,37 +81,45 @@ const packageWasmExports =
         setProperty(hostFns, libName, !!hostFns[libName] ? hostFns[libName] : {})
       )
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Instantiate a sequence of WASM modules that potentially have import dependencies on some predecessor in the list
-const instantiateWasmModuleSequence = async moduleSequence => {
+/***********************************************************************************************************************
+ * Instantiate a list of WASM modules that potentially have import dependencies on some previously instantiated module
+ *
+ * @param {WasmModule[]} moduleSequence - A list of WASM modules in the order in which they need to be instantiated
+ * @param {object}       initialHostFns - The object containing any host functions imported by the first WASM module
+ *
+ * @returns {object} An object containing a `hostFunctions` property.  This contains all the functions exported by the
+ *                   instantiated WASM modules
+ */
+const instantiateWasmModuleSequence = async (moduleSequence, initialHostFns) => {
   let idx = 0
   let wasmModAccumulator = {}
 
-  wasmModAccumulator.hostFunctions = moduleSequence[0].hostFunctions
+  wasmModAccumulator.hostFunctions = initialHostFns
 
   for (idx=0; idx < moduleSequence.length; idx++) {
     const thisMod = moduleSequence[idx]
 
-    console.log(`Instantiating ${thisMod.wasmBin}`)
+    console.log(`Instantiating ${thisMod.pathToWasmBin}`)
 
-    let wasmObj = await WebAssembly.instantiateStreaming(fetch(thisMod.wasmBin), wasmModAccumulator.hostFunctions)
+    let wasmObj = await WebAssembly.instantiateStreaming(fetch(thisMod.pathToWasmBin), wasmModAccumulator.hostFunctions)
 
     wasmModAccumulator.hostFunctions = packageWasmExports(
       wasmObj.instance,
-      thisMod.addWasmExportsToLibName,
+      thisMod.exportToLib,
       wasmModAccumulator.hostFunctions
     )
 
-    wasmModAccumulator[thisMod.addWasmExportsToLibName] = wasmObj
+    wasmModAccumulator[thisMod.exportToLib] = wasmObj
   }
 
   return wasmModAccumulator
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Public API
-// ---------------------------------------------------------------------------------------------------------------------
+/***********************************************************************************************************************
+ * Public API
+ */
 export {
+  WasmModule,
   instantiateWasmModuleSequence,
 
   F64,
